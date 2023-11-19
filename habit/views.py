5ba3e39b2244ff94201f410d29ea16c9be51bcf2
kelllyy101 <kellyom81@hotmail.com>
2023-11-19 22:12:05 +0000

    rendering data  and add_habit.html

--- habit/views.py
+++ habit/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
-def get_habits(request):
-    return render(request, 'habits/habits.html')
 
+
+def get_habits(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'habits/habits.html', context)
