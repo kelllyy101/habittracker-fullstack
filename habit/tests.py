e6fa7d878691ee519c1c21819a95bfb6a1eeaa69
kelllyy101 <kellyom81@hotmail.com>
2023-12-01 19:31:06 +0000

    tests for add and delete functionality of the habits

--- habit/tests.py
+++ habit/tests.py
@@ -16,11 +16,23 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'habits/add_habit.html')
     
 
-    def test_get_edit_item_page(self):
+    def test_edit_habit(self):
         item = Item.objects.create(name='Test Habit')
         response = self.client.get(f'/edit/{item.id}')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'habit/edit_habit.html')
+        self.assertTemplateUsed(response, 'habits/edit_habit.html')
+
+    
+    def test_can_add_habit(self):
+        response = self.client.post('/add', {'name': 'Test Added Habit'})
+        self.assertRedirects(response, '/')
+
+    
+    def test_can_delete_habit(self):
+        item = Item.objects.create(name='Test Todo Habit')
+        response = self.client.get(f'/delete/{item.id}')
+
+
 
 
 
