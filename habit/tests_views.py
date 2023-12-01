741fcc1cedcf2ec2ae55c2d5367dd7d733653dc2
kelllyy101 <kellyom81@hotmail.com>
2023-12-01 23:12:11 +0000

    updated tests

--- habit/tests_views.py
+++ habit/tests_views.py
@@ -6,17 +6,20 @@ from .models import Item
 class TestViews(TestCase):
 
     def test_habit_list(self):
+        # Test to view/get habits
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'habits/habits.html')
 
     def test_get_add_habit_page(self):
+        # Test to add habits from add_habit.html and it saves
         response = self.client.get('/add')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'habits/add_habit.html')
     
 
     def test_edit_habit(self):
+        # Test to edit habit and it saves
         item = Item.objects.create(name='Test Habit')
         response = self.client.get(f'/edit/{item.id}')
         self.assertEqual(response.status_code, 200)
@@ -29,6 +32,7 @@ class TestViews(TestCase):
 
     
     def test_can_delete_habit(self):
+        # Test that deleted habits are deleted
         item = Item.objects.create(name='Test Todo Habit')
         response = self.client.get(f'/delete/{item.id}')
         self.assertRedirects(response, '/')
@@ -37,6 +41,7 @@ class TestViews(TestCase):
 
 
     def test_can_toggle_habit(self):
+        # Test to toggle habit
         item = Item.objects.create(name='Test Habit', done=True)
         response = self.client.get(f'/toggle/{item.id}')
         self.assertRedirects(response, '/')
@@ -45,30 +50,21 @@ class TestViews(TestCase):
 
 
     def test_can_edit_habit_redirect(self):
+        # Test redirect to main page / get_habits after habit is updated
         item = Item.objects.create(name='Test Habit')
         response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
         self.assertRedirects(response, '/')
         updated_item = Item.objects.get(id=item.id)
         self.assertEqual(updated_item.name, 'Updated Name')
 
+
     def test_add_habit_view_get(self):
         # Test the GET request for the add_habit view
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'habits/habits.html')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], Item)
 
-    def test_add_habit_view_post_valid_form(self):
-        # Test the POST request with a valid form submission
-        response = self.client.post(reverse('add_habit'), data)
-        self.assertEqual(response.status_code, 302)  # 302 is the status code for a redirect
-        self.assertRedirects(response, reverse('get_habits'))
-
-        # Verify that the habit was added to the database
-        self.assertEqual(Item.objects.count(), 1)
-        habit = Item.objects.get()
-        self.assertEqual(habit.name, 'Exercise')
-        self.assertEqual(habit.description, 'Daily workout')
 
     def test_add_habit_view_post_invalid_form(self):
         # Test the POST request with an invalid form submission
