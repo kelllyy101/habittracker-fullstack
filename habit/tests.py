6afb17f8e645cef1530ca9ad1698922de4fe37a9
kelllyy101 <kellyom81@hotmail.com>
2023-12-01 19:25:08 +0000

    added two tests to test views.py

--- habit/tests.py
+++ habit/tests.py
@@ -8,5 +8,19 @@ class TestViews(TestCase):
     def test_habit_list(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'habit/habits.html')
+        self.assertTemplateUsed(response, 'habits/habits.html')
+
+    def test_get_add_habit_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'habits/add_habit.html')
+    
+
+    def test_get_edit_item_page(self):
+        item = Item.objects.create(name='Test Habit')
+        response = self.client.get(f'/edit/{item.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'habit/edit_habit.html')
+
+
 
