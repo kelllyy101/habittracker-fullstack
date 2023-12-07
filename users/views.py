359794101e7a3efbdbc42109bb9900690b0f6413
kelllyy101 <kellyom81@hotmail.com>
2023-12-07 21:41:56 +0000

    login html created

--- users/views.py
+++ users/views.py
@@ -19,27 +19,3 @@ def login_user(request):
 
 	else:
 		return render(request, 'authenticate/login.html', {})
-
-def logout_user(request):
-	logout(request)
-	messages.success(request, ("You Were Logged Out!"))
-	return redirect('home')
-
-
-def register_user(request):
-	if request.method == "POST":
-		form = RegisterUserForm(request.POST)
-		if form.is_valid():
-			form.save()
-			username = form.cleaned_data['username']
-			password = form.cleaned_data['password1']
-			user = authenticate(username=username, password=password)
-			login(request, user)
-			messages.success(request, ("Registration Successful!"))
-			return redirect('home')
-	else:
-		form = RegisterUserForm()
-
-	return render(request, 'authenticate/register_user.html', {
-		'form':form,
-		})
