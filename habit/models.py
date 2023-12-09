2cdab686bbce6bbf5f5844aaed7e60c0ea66012d
kelllyy101 <kellyom81@hotmail.com>
2023-12-09 22:13:26 +0000

    add modified classes to get more infomation from the suer

--- habit/models.py
+++ habit/models.py
@@ -6,8 +6,19 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+    description = models.TextField(max_length=200, blank=True)
+    frequency = models.IntegerField(null=True, blank=True)
     
     def __str__(self):
         return self.name
 
+
+class HabitUsers(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField('User Email')
+    shared_users = models.ManyToManyField(Item, blank=True)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
 
