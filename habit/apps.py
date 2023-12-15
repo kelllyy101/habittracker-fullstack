8402168caa2b4005639ea9f39eb1edbf24e0eed0
kelllyy101 <kellyom81@hotmail.com>
2023-12-15 23:52:10 +0000

    Revert "added habit display function"
    This reverts commit c5d496a34c9218fe8febdf33c62f71e5ba71f092.

--- habit/apps.py
+++ habit/apps.py
@@ -4,13 +4,3 @@ from django.apps import AppConfig
 class HabitConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'habit'
-
-
-def habits_display(habits):
-    return [
-        {
-            "id": habit.id,
-            "name": habit.description,
-            "frequency": " - ".join([days[int(day)] for day in habit.frequency.split()])
-        } for habit in habits
-    ]
