c693d403e1aff8af659c09900f37289774270a1e
kelllyy101 <kellyom81@hotmail.com>
2023-12-20 23:35:13 +0000

    add hosts

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-y1!b!wucm$b)l$+r21r^mis+wai=zl+55t_hz@*xby%kj^e+5m
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fs-habit-tracker-786d6f38cbab.herokuapp.com']
 
 
 LOGIN_URL = "login"
